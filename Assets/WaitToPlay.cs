54051e936a0f42e3e047eca01f18ef20ad467b2e
lucas3311 <lucaslimareis3311@gmail.com>
2019-06-01 14:01:42 -0300

    3 paginas sonorizadas

--- Assets/WaitToPlay.cs
+++ Assets/WaitToPlay.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaitToPlay : MonoBehaviour {
-
+    public float secondsToWait = 0;
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(waitPlay(secondsToWait));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private IEnumerator waitPlay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        gameObject.GetComponent<AudioSource>().Play();
+    }
 }
