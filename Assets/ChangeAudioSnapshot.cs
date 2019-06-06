b62c2f6614032c4be3a479b314ec2afcc24fe577
lucas3311 <lucaslimareis3311@gmail.com>
2019-06-06 01:23:04 -0300

    audio mixers

---
+++ Assets/ChangeAudioSnapshot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ChangeAudioSnapshot : MonoBehaviour {
+    public AudioMixerSnapshot Snapshot;
+    public float SecondsToTransition;
+	// Use this for initialization
+	void Start () {
+        Snapshot.TransitionTo(SecondsToTransition);
+	}
+}
